7b11aecc842d01167f7923100ab9cae99b0daaaf
argen23 <argenomarbekov192@gmail.com>
2021-06-22 19:08:39 +0300

    lox

--- app/src/main/java/com/example/a11/MainActivity2.java
+++ app/src/main/java/com/example/a11/MainActivity2.java
@@ -1,18 +1,19 @@
 package com.example.a11;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-
 public class MainActivity2 extends AppCompatActivity {
     private Button button;
     private TextView tv;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
@@ -22,7 +23,15 @@ public class MainActivity2 extends AppCompatActivity {
         button = findViewById(R.id.go);
         button.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity2.this,MainActivity3.class);
-            startActivity(intent);
+            startActivityForResult(intent,1);
         });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            String result = data.getStringExtra("text");
+            tv.setText(result);
+        }
     }
 }
--- app/src/main/java/com/example/a11/MainActivity3.java
+++ app/src/main/java/com/example/a11/MainActivity3.java
@@ -1,7 +1,9 @@
 package com.example.a11;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,19 +11,24 @@ import android.widget.EditText;
 
 public class MainActivity3 extends AppCompatActivity {
     private EditText et;
-    public  static final String TEXT =  "text";
+    public static final String TEXT = "text";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
         et = findViewById(R.id.et_num);
         findViewById(R.id.change_btn).setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity3.this, MainActivity2.class);
+            Intent intent = new Intent();
             String text = et.getText().toString();
-            if (!text.isEmpty()){
-            intent.putExtra(TEXT,text);
-            startActivity(intent);
+            if (!text.isEmpty()) {
+                intent.putExtra("text", text);
+                setResult(RESULT_OK, intent);
+                finish();
             }
         });
     }
+
+
 }
